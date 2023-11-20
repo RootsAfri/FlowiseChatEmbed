7f8ed96d35aed4c930c398021edcdf9780fd37a6
rootsafrikiko <rootsafrikiko@gmail.com>
2023-11-19 21:11:56 -0500

    Remove Branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,14 +54,14 @@ export const Badge = (props: Props) => {
     }}>Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://aiolosmedia.com'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Aiolos Media</span>
       </a>
     </span>
   )
